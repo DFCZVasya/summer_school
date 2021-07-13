0498c4e84274c8b3170b1bc5be11d458d331753c
VKI <123@mail.com>
2021-07-13 13:32:09 +0700

    day7

--- day7/calculator.py
+++ day7/calculator.py
@@ -1,12 +1,15 @@
 from tkinter import *
 
 def add_digit(digit):
-    value = calc.get() + str(digit)
+    value = calc.get() + digit
     calc.delete(0, END)
     calc.insert(0, value)
 
 def make_digit_button(digit):
-    return Button(text=str(digit), font = ('Arial', 20), bd = 2, command=lambda : add_digit(digit))
+    return Button(text=digit, font = ('Arial', 20), bd = 2, command=lambda : add_digit(digit))
+
+def make_operation_button(operation):
+    return Button(text=operation, font = ('Arial', 20), bd = 2, command=lambda : add_digit(operation))
 
 root = Tk()
 root.title("calculator")
@@ -15,16 +18,16 @@ root.geometry("240x260")
 calc = Entry(root, justify=RIGHT, font = ('Arial', 15), width= 15)
 calc.grid(row = 0, column = 0, columnspan=3, stick = 'we')
 
-make_digit_button(1).grid(row = 1, column=0, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(2).grid(row = 1, column=1, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(3).grid(row = 1, column=2, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(4).grid(row = 2, column=0, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(5).grid(row = 2, column=1, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(6).grid(row = 2, column=2, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(7).grid(row = 3, column=0, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(8).grid(row = 3, column=1, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(9).grid(row = 3, column=2, stick = 'wens', padx = 2, pady = 2)
-make_digit_button(0).grid(row = 4, column=1, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('1').grid(row = 1, column=0, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('2').grid(row = 1, column=1, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('3').grid(row = 1, column=2, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('4').grid(row = 2, column=0, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('5').grid(row = 2, column=1, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('6').grid(row = 2, column=2, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('7').grid(row = 3, column=0, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('8').grid(row = 3, column=1, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('9').grid(row = 3, column=2, stick = 'wens', padx = 2, pady = 2)
+make_digit_button('0').grid(row = 4, column=1, stick = 'wens', padx = 2, pady = 2)
 
 root.grid_columnconfigure(0,minsize = 60)
 root.grid_columnconfigure(1,minsize = 60)
