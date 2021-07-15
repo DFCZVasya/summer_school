d036e9044ee6c53f7d3d9c3b1cf4415090ab1e6a
VKI <123@mail.com>
2021-07-15 11:57:17 +0700

    day9

--- day7/game2.py
+++ day7/game2.py
@@ -37,14 +37,32 @@ pygame.image.load('pygame_left_6.png')]
 playerStand = pygame.image.load('pygame_idle.png')
 bg = pygame.image.load('pygame_bg.jpg')
 
+clock = pygame.time.Clock()
+
 def draw_window():
+    global animCount
     win.blit(bg, (0,0))
-    pygame.draw.rect(win, (0,255,0), (x, y, width, height))
-    pygame.draw.rect(win, (150,75,0), (0, 480, 500, 20))
+    if animCount + 1 >= 30:
+        animCount = 0
+    
+    if left:
+        win.blit(walkLeft[animCount // 5], (x, y))
+        animCount += 1
+    elif right:
+        win.blit(walkRight[animCount // 5], (x, y))
+        animCount += 1
+    else:
+        win.blit(playerStand, (x,y))
+
+    #pygame.draw.rect(win, (0,255,0), (x, y, width, height))
+    #pygame.draw.rect(win, (150,75,0), (0, 480, 500, 20))
+
+
 
 run = True
 while run:
-    pygame.time.delay(50)
+    #pygame.time.delay(50)
+    clock.tick(30)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -52,9 +70,18 @@ while run:
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > 5:
+        left = True
+        right = False
         x -= speed
-    if keys[pygame.K_RIGHT] and x < 500 - width - 5:
+    elif keys[pygame.K_RIGHT] and x < 500 - width - 5:
+        left = False
+        right = True
         x += speed
+    else:
+        left = False
+        right = False
+        animCount = 0
+    
     if not isJump:
         if keys[pygame.K_UP] and y > 5:
             isJump = True
