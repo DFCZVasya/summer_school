7e3f98815cf7a4037d20db7865ced241962b425c
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2022-07-06 13:03:18 +0700

    Add files via upload

---
+++ sadas.py
@@ -0,0 +1,34 @@
+users = [["test", "test"], ["admin", "admin"]]
+
+
+def registration():
+    name = input("enter name: ")
+    password1 = input("enter password: ")
+    password2 = input("enter password again: ")
+    if password1 == password2:
+        print("passwords are not equal!")
+        return False
+
+    for user in users:
+        if name == user[0]:
+            print("name is already taken!")
+            return False
+
+
+    users.append([name, password1])
+    return True    
+
+def login():
+    name = input("enter name: ")
+    password = input("enter password: ")
+    for user in users:
+        if name == user[0] and password == user[1]:
+            return True
+    return False
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
