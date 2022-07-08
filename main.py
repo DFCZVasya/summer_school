3db8f361789a8797ba747d2dc5576d65598e73f8
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2022-07-08 13:55:36 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ for i in range(0,3):
     A.append([])
     for j in range(0,3):
         A[i].append(random.randint(0,10))
-        
+
 matrixPrint.printMatrix(A)
+
+print(
+    A[0][0]*(A[1][1]*A[2][2]-A[1][2]*A[2][1])
+    -A[0][1]*(A[1][0]*A[2][2]-A[1][2]*A[2][0])
+    +A[0][2]*(A[1][0]*A[2][1]-A[1][1]*A[2][0]))
