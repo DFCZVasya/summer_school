1f3d3cfd9c00cc352dca4f6ed505de8ebb9c2d51
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2022-07-13 13:17:00 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode([500,500])
     running = True
-    pygame.draw.rect(screen, (255,0,0), pygame.Rect(100,100,100,100))
+    x = 100
+    y = 100
+    dy = 5
+    pygame.draw.rect(screen, (255,0,0), pygame.Rect(x,y,100,100))
     pygame.display.flip()
     while running:
         for event in pygame.event.get():
             print(event)
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    y += dy
+                    pygame.draw.rect(screen, (255,0,0), pygame.Rect(x,y,100,100))
+                    pygame.display.flip()
     pygame.quit()
 
 if __name__ == "__main__":
