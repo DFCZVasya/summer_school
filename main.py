d80e33a507c0f4e2824d9cd512936386e063d5cc
Vasya Lopatkin <48513210+DFCZVasya@users.noreply.github.com>
2022-07-14 10:54:47 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import pygame
 
 width = 500
 height = 500 
-
+x = 100
+y = 100
+dy = 2
+dx = 2
+screen = pygame.display.set_mode([width,height])
 bg = pygame.image.load("img/pygame_bg.jpg")
 
 player_stand = pygame.image.load("img/pygame_idle.png")
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode([width,height])
-    running = True
-    x = 100
-    y = 100
-    dy = 2
-    dx = 2
-
+def drawWindow():
+    screen.blit(bg,(0,0))
+    screen.blit(player_stand,(x,y))
     pygame.display.update()
+
+def main():
+    global x,y,dx,dy
+    pygame.init()
+    running = True
     while running:
-        screen.blit(bg,(0,0))
         pygame.time.delay(10)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -33,9 +35,7 @@ def main():
             y -= dy
         if keys[pygame.K_DOWN] and y < 429:
             y += dy
-
-        screen.blit(player_stand,(x,y))
-        pygame.display.update()
+        drawWindow() 
     pygame.quit()
 
 if __name__ == "__main__":
